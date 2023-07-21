75f08e7e89e4980f20d3aee85f0b6b39a5685932
harsh-agr79 <agrharsh7932@gmail.com>
2023-07-21 17:54:58 +0545

    changes

--- resources/views/admin/customers.blade.php
+++ resources/views/admin/customers.blade.php
@@ -35,7 +35,7 @@
                 </thead>
                 <tbody>
                     @foreach ($data as $item)
-                        <tr>
+                        <tr  oncontextmenu="rightmenu({{ $item->id }}); return false;">
                             <td>{{ $item->name }}</td>
                             <td>{{ $item->shopname }}</td>
                             <td>{{ $item->address }}</td>
@@ -49,6 +49,17 @@
                 </tbody>
             </table>
         </div>
+    </div>
+
+    <div id="rightmenu" class="rmenu">
+        <ul>
+            <a id="rmeditlink">
+                <li>Edit</li>
+            </a>
+            <a id="rmdeletelink">
+                <li class="border-top">Delete</li>
+            </a>
+        </ul>
     </div>
 
     <script>
@@ -101,6 +112,51 @@
             vals.forEach(e => {
                 $(`.${e}`).show();
             });
+        }
+    </script>
+    <script>
+           function rightmenu(id) {
+            // console.log(orderid)
+            var rmenu = document.getElementById("rightmenu");
+            var perms = @json($perms);
+            var admintype = `{{ $admin->type }}`;
+            if (admintype == "admin") {
+                rmenu.style.display = 'block';
+                rmenu.style.top = mouseY(event) + 'px';
+                rmenu.style.left = mouseX(event) + 'px';
+                $('#rmeditlink').attr('href', '/editcustomer/' + id);
+                $('#rmdeletelink').attr('href', '/deletecustomer/' + id);
+            }
+        }
+
+        $(document).bind("click", function(event) {
+            var rmenu = document.getElementById("rightmenu");
+            rmenu.style.display = 'none';
+        });
+
+        function mouseX(evt) {
+            if (evt.pageX) {
+                return evt.pageX;
+            } else if (evt.clientX) {
+                return evt.clientX + (document.documentElement.scrollLeft ?
+                    document.documentElement.scrollLeft :
+                    document.body.scrollLeft);
+            } else {
+                return null;
+            }
+        }
+
+        // Set Top Style Proparty
+        function mouseY(evt) {
+            if (evt.pageY) {
+                return evt.pageY;
+            } else if (evt.clientY) {
+                return evt.clientY + (document.documentElement.scrollTop ?
+                    document.documentElement.scrollTop :
+                    document.body.scrollTop);
+            } else {
+                return null;
+            }
         }
     </script>
 @endsection
