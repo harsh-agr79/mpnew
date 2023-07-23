abb371693383dc161843cac89d07c0f50d08416a
harsh-agr79 <agrharsh7932@gmail.com>
2023-07-23 13:21:16 +0545

    changes

--- resources/views/admin/sortanalytics.blade.php
+++ resources/views/admin/sortanalytics.blade.php
@@ -14,13 +14,19 @@
                         <input type="date" name="date2" value="{{ $date2 }}"
                             class="inp browser-default black-text">
                     </div>
-                    <div class="input-field col s6 l6">
+                    <div class="input-field col s6 l4 m4">
                         <input type="text" name="name" id="customer" value="{{ $name }}"
                             placeholder="Customer" class="autocomplete browser-default inp black-text" autocomplete="off">
                     </div>
-                    <div class="input-field col s6 l6">
+                    <div class="input-field col s6 l4 m4">
                         <input type="text" name="product" id="product" value="{{ $product }}"
                             placeholder="Product" class="autocomplete browser-default inp black-text" autocomplete="off">
+                    </div>
+                    <div class='input-field col l4 m4 s12'>
+                        <input class='validate browser-default inp search black-text z-depth-1' onkeyup="searchFun()" autocomplete="off"
+                            type='search' name='search' id='search' />
+                        <span class="field-icon" id="close-search"><span class="material-icons"
+                                id="cs-icon">search</span></span>
                     </div>
                     <div class="col s6 m2 l2">
                         <button class="btn amber darken-1">Apply</button>
@@ -384,5 +390,44 @@
                 }
             })
         })
+    </script>
+    <script>
+        const searchFun = () => {
+            var filter = $('#search').val().toLowerCase();
+            const a = document.getElementById('search');
+            const clsBtn = document.getElementById('close-search');
+            let table = document.getElementsByTagName('table');
+            let tr = $('tr')
+            clsBtn.addEventListener("click", function() {
+                a.value = '';
+                a.focus();
+                var filter = '';
+                for (var i = 0; i < tr.length; i++) {
+                    tr[i].style.display = "";
+                }
+                $('#cs-icon').text('search')
+            });
+            if (filter === '') {
+                $('#cs-icon').text('search')
+            } else {
+                $('#cs-icon').text('close')
+            }
+
+            for (var i = 0; i < tr.length; i++) {
+                let td = tr[i].getElementsByTagName('td');
+                // console.log(td);
+                for (var j = 0; j < td.length; j++) {
+                    if (td[j]) {
+                        let textvalue = td[j].textContent || td[j].innerHTML;
+                        if (textvalue.toLowerCase().indexOf(filter) > -1) {
+                            tr[i].style.display = "";
+                            break;
+                        } else {
+                            tr[i].style.display = "none"
+                        }
+                    }
+                }
+            }
+        }
     </script>
 @endsection
