52eb0ff1ba95ba412b685779b9bbd1d47e864fc6
harsh-agr79 <agrharsh7932@gmail.com>
2023-07-28 15:21:21 +0545

    changes

--- app/Http/Middleware/MarketerAuth.php
+++ app/Http/Middleware/MarketerAuth.php
@@ -17,9 +17,16 @@ class MarketerAuth
     public function handle(Request $request, Closure $next): Response
     {
         if($request->session()->has('ADMIN_LOGIN') && in_array(session()->get('ADMIN_TYPE'), ['marketer'])){
-            
+            $time = session()->get('ADMIN_TIME');
+            $cutime = time();
+
+            if($cutime - $time <= 3600){
             view()->share('admin', DB::table('admins')->where('id', session()->get('ADMIN_ID'))->first());
             view()->share('perms', DB::table('permission')->where('userid', session()->get('ADMIN_ID'))->pluck('perm')->toArray());
+            }
+            else{
+                return redirect('/logout');
+            }
         }
         else{
             $request->session()->flash('error','Access Denied');
