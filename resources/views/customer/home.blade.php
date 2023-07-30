a46091418ee09b7792435e024215bc1af0b011d6
harsh-agr79 <agrharsh7932@gmail.com>
2023-07-30 22:35:26 +0545

    changes

--- resources/views/customer/home.blade.php
+++ resources/views/customer/home.blade.php
@@ -42,7 +42,7 @@
               </section>
           </div>
         </div>
-        <div class="col l6 m12 s12 row center" style="margin-top: 20px;">
+        <div class="col l6 m12 s12 row center" style="margin-top: 5px;">
             <div class="col s12" style="margin-top: 10px;">
                 <a href="{{ url('user/createorder') }}" class="home-btn spc">Create A New Order<i
                         class="material-icons">add</i></a>
