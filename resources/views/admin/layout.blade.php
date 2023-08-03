088f802a51a0ba60fe2f5ecd20a8ffb8ebd42c40
harsh-agr79 <agrharsh7932@gmail.com>
2023-08-03 19:36:15 +0545

    changes

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -162,6 +162,10 @@
                                         <li><a href="{{ url('/detailedreport') }}" class="textcol">Detailed Report<i
                                                     class="material-icons textcol">show_chart</i></a></li>
                                     @endif
+                                    @if ($admin->type == 'admin'))
+                                    <li><a href="{{ url('/productreport') }}" class="textcol">Product Report<i
+                                                class="material-icons textcol">show_chart</i></a></li>
+                                    @endif
                                 </ul>
                             </div>
                         </li>
