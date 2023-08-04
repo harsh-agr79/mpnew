b4bef2c440cd5fb7ea0c34c9376f000d869d24d9
harsh-agr79 <agrharsh7932@gmail.com>
2023-08-04 13:01:52 +0545

    changes

--- resources/views/admin/productreport.blade.php
+++ resources/views/admin/productreport.blade.php
@@ -208,7 +208,7 @@
                             <th>Date/Category</th>
                             @foreach ($data as $item)
                                 <th @if ($item->hide == 'on') style="display: none;" class="hidden" @endif>
-                                    <span>{{ $item->name }}</span>
+                                    <span class="truncate">{{ $item->name }}</span>
                                 </th>
                             @endforeach
                         </tr>
