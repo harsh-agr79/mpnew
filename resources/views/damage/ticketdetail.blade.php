4dd2bc5664e63e37987dd58838cf2cafba4d0224
harsh-agr79 <agrharsh7932@gmail.com>
2023-10-06 22:18:30 +0530

    Details page change

--- resources/views/damage/ticketdetail.blade.php
+++ resources/views/damage/ticketdetail.blade.php
@@ -7,44 +7,45 @@
             <div>Date: {{ $data[0]->date }}</div>
             <div>invoiceid: {{ $data[0]->invoiceid }}</div>
         </div>
-        <div class="mp-card" style="margin-top: 10px;">
+        <div>
             @foreach ($data as $item)
-                <div class="center row">
-                    <div class="col s4">Item: {{ $item->item }}</div>
-                    <div class="col s4">Quantity: {{ $item->quantity }}</div>
-                    <div class="col s4">Your Remarks: {{ $item->cusremarks }}</div>
+                <div class="mp-card" style="margin-top: 10px;">
+                    <div class="center row">
+                        <div class="col s4">Item: {{ $item->item }}</div>
+                        <div class="col s4">Quantity: {{ $item->quantity }}</div>
+                        <div class="col s4">Customer Remarks: {{ $item->cusremarks }}</div>
+                    </div>
+                    @php
+                        
+                        $prod = DB::table('damage')
+                            ->where('invoiceid', $item->invoiceid)
+                            ->where('item', $item->item)
+                            ->get();
+                        
+                    @endphp
+                    <table>
+                        <thead>
+                            <th>Group Quantity</th>
+                            <th>Condition</th>
+                            <th>Warranty Status</th>
+                            <th>Problem</th>
+                            <th>Solution</th>
+                            <th>Remarks</th>
+                        </thead>
+                        <tbody>
+                            @foreach ($prod as $item2)
+                                <tr>
+                                    <td>{{ $item2->grpqty }}</td>
+                                    <td>{{ $item2->condition }}</td>
+                                    <td>{{ $item2->warranty }}</td>
+                                    <td>{{ $item2->problem }}</td>
+                                    <td>{{ $item2->solution }}</td>
+                                    <td>{{ $item2->adremarks }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
-                @php
-                    
-                    $prod = DB::table('damage')
-                        ->where('invoiceid', $item->invoiceid)
-                        ->where('item', $item->item)
-                        ->get();
-                    
-                @endphp
-                <table>
-                    <thead>
-                        <th>Group Quantity</th>
-                        <th>Condition</th>
-                        <th>Warranty Status</th>
-                        <th>Problem</th>
-                        <th>Solution</th>
-                        <th>Remarks</th>
-                    </thead>
-                    <tbody>
-                        @foreach ($prod as $item2)
-                        <tr>
-                            <td>{{$item2->grpqty}}</td>
-                            <td>{{$item2->condition}}</td>
-                            <td>{{$item2->warranty}}</td>
-                            <td>{{$item2->problem}}</td>
-                            <td>{{$item2->solution}}</td>
-                            <td>{{$item2->adremarks}}</td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-              
             @endforeach
         </div>
     </div>
