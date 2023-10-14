4404e82d9c5ed972b9dca9b871343890478bf5eb
harsh-agr79 <agrharsh7932@gmail.com>
2023-10-14 18:13:59 +0530

    changes

--- resources/views/damage/tickets.blade.php
+++ resources/views/damage/tickets.blade.php
@@ -8,19 +8,26 @@
         <div class="center">
             <h5>Damage Tickets</h5>
         </div>
+        <div class="row">
+            <div class='input-field col s12' style="margin:0; padding: 5px;">
+                <input class='validate browser-default inp search black-text z-depth-1' onkeyup="searchFun()"
+                    autocomplete="off" type='search' id='search' />
+                <span class="field-icon" id="close-search"><span class="material-icons" id="cs-icon">search</span></span>
+            </div>
+        </div>
         <div class="mp-card" style="margin-top: 20px;">
-            <ul class="collapsible">
+            <ul class="collapsible dmg-container">
                 @foreach ($data as $item)
-                    <li>
+                    <li class="dmg-inv">
                         <div class="collapsible-header row">
                             <div class="col s1">
                                 <div style="height: 20px; width: 10px;" class="{{ tktcolor($item->invoiceid) }}"></div>
                             </div>
-                            <div class="col s2">{{ $item->date }}</div>
-                            <div class="col s3">{{ $item->name }}</div>
-                            <div class="col s3"><span id="{{ $item->invoiceid }}mainstat">{{ $item->mainstatus }}</span>
+                            <div class="col s2 dmg-inf">{{ $item->date }}</div>
+                            <div class="col s3 dmg-inf">{{ $item->name }}</div>
+                            <div class="col s3 dmg-inf"><span id="{{ $item->invoiceid }}mainstat">{{ $item->mainstatus }}</span>
                             </div>
-                            <div class="col s3">{{ $item->invoiceid }}</div>
+                            <div class="col s3 dmg-inf">{{ $item->invoiceid }}</div>
                         </div>
                         <div class="collapsible-body"><span>
                                 <div class="right">
@@ -113,7 +120,8 @@
             <form class="center" id="dcform">
                 <input type="hidden" id="dateinv" name="inv">
                 <input type="hidden" id="datestat" name="stat">
-                <input type="datetime-local" datetime="YYYY-MM-DD hh:mm:ss" step="any" name="date" id="datedate" class="browser-default inp">
+                <input type="datetime-local" datetime="YYYY-MM-DD hh:mm:ss" step="any" name="date" id="datedate"
+                    class="browser-default inp">
                 <button class="amber btn" style="margin-top: 20px;">Submit</button>
             </form>
         </div>
@@ -186,23 +194,63 @@
             $('#datedate').val(date)
         }
         $('#dcform').on('submit', function(e) {
-                    e.preventDefault();
-                    let formData = new FormData($("#dcform")[0]);
-                    $.ajax({
-                        headers: {
-                            "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
-                        },
-                        url: "/ticket/changedate",
-                        data: formData,
-                        contentType: false,
-                        processData: false,
-                        type: "POST",
-                        success: function(response) {
-                            console.log(response)
-                            $(`#${response.inv+response.stat}lbl`).text(response.date);
-                            $('#datechange').modal('close');
+            e.preventDefault();
+            let formData = new FormData($("#dcform")[0]);
+            $.ajax({
+                headers: {
+                    "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
+                },
+                url: "/ticket/changedate",
+                data: formData,
+                contentType: false,
+                processData: false,
+                type: "POST",
+                success: function(response) {
+                    console.log(response)
+                    $(`#${response.inv+response.stat}lbl`).text(response.date);
+                    $('#datechange').modal('close');
+                }
+            })
+        })
+        const searchFun = () => {
+            var filter = $('#search').val().toLowerCase();
+            const a = document.getElementById('search');
+            const clsBtn = document.getElementById('close-search');
+            let cont = document.getElementsByClassName('dmg-container');
+            let prod = $(".dmg-inv")
+
+            clsBtn.addEventListener("click", function() {
+                a.value = '';
+                a.focus();
+                var filter = '';
+                for (var i = 0; i < prod.length; i++) {
+                    prod[i].style.display = "";
+                }
+                $('#cs-icon').text('search')
+            });
+            if (filter === '') {
+                $('#cs-icon').text('search')
+            } else {
+                $('#cs-icon').text('close')
+            }
+            // console.log(prod);
+
+            for (var i = 0; i < prod.length; i++) {
+                let span = prod[i].getElementsByClassName('dmg-inf');
+                // console.log(td);
+                for (var j = 0; j < span.length; j++) {
+                    if (span[j]) {
+                        let textvalue = span[j].textContent || span[j].innerHTML;
+                        if (textvalue.toLowerCase().indexOf(filter) > -1) {
+                            prod[i].style.display = "";
+                            break;
+                        } else {
+                            prod[i].style.display = "none"
                         }
-                    })
-                })
+                    }
+                }
+            }
+            // toggleinactive();
+        }
     </script>
 @endsection
