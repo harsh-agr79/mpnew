7ed611c8688f6d89f37588f9acdacb1fd1b9f76e
harsh-agr79 <agrharsh7932@gmail.com>
2024-02-11 21:59:38 +0530

    error page

---
+++ resources/views/errorload.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  </head>
+
+  <body>
+    <div class="container">
+        <div class="valign-wrapper" style="height: 100vh">
+            <div>
+                <h1>There seems to be a problem with the server</h1>
+                <h3>Please try again later</h3>
+                <h2>:/</h2>
+            </div>
+        </div>
+    </div>
+    <!--JavaScript at end of body for optimized loading-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  </body>
+</html>
+      
